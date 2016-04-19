5a4f71d3c254b6d83a9cd527dc2bacda089eac8a
Charles R Walker Jr <crwalkerjr@gmail.com>
2016-04-19 08:02:02 -0600

    really finished devise now

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,10 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Require for Heroku
+  config.action_mailer.default_url_options = { host: 'onemonth-bubs.herokuapp.com' }
+
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
